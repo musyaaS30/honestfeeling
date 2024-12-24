5062add2f31210e597deddc05d55bf328177e537
musyaaS30 <mussforrttk@gmail.com>
2024-12-24 13:48:33 +0700

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function playSound() {
     const sound = document.getElementById("sound");
     sound.play();
 }
+const image = document.getElementById('image');
+
+    image.addEventListener('click', () => {
+      // Tambahkan class 'clicked' untuk memicu animasi
+      image.classList.add('clicked');
+
+      // Hapus class setelah animasi selesai agar bisa diulang
+      setTimeout(() => {
+        image.classList.remove('clicked');
+      }, 500); // Durasi animasi 0.5 detik
+    });
